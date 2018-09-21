536efe98e413133f9b66e95cd358a3b0d7c17da8
cdn64 <gitcdn@gmail.com>
2018-09-21 17:44:48 +0200

    Location and movement, refactoring

--- crypt.ino
+++ crypt.ino
@@ -1,5 +1,52 @@
 /*
- * Button Interaction */
+ * Global State
+ */
+
+// Orientation
+
+#define NORTH 0
+#define WEST 1
+#define SOUTH 2
+#define EAST 3
+
+int previousOrientation = -1;
+int orientation = NORTH; // 0-3
+
+// Location
+
+int currentLocation[] = {0, 0};
+int previousLocation[] = {0, 0};
+
+// «Things» on/off the map
+
+#define WALL -1
+#define EMPTY 0
+#define SPECIAL 1
+
+// Map
+
+#define WIDTH 8
+#define LENGTH 8
+int _map[WIDTH][LENGTH] = {
+	{EMPTY, EMPTY, EMPTY, EMPTY,   EMPTY, EMPTY, EMPTY, EMPTY},
+	{EMPTY, EMPTY, EMPTY, EMPTY,   EMPTY, EMPTY, EMPTY, EMPTY},
+	{EMPTY, EMPTY, EMPTY, EMPTY,   EMPTY, EMPTY, EMPTY, EMPTY},
+	{EMPTY, EMPTY, EMPTY, EMPTY,   EMPTY, EMPTY, EMPTY, EMPTY},
+	{EMPTY, EMPTY, EMPTY, SPECIAL, EMPTY, EMPTY, EMPTY, EMPTY},
+	{EMPTY, EMPTY, EMPTY, EMPTY,   EMPTY, EMPTY, EMPTY, EMPTY},
+	{EMPTY, EMPTY, EMPTY, EMPTY,   EMPTY, EMPTY, EMPTY, EMPTY},
+	{EMPTY, EMPTY, EMPTY, EMPTY,   EMPTY, EMPTY, EMPTY, EMPTY}
+};
+
+// Error
+
+#define NO_ERROR -1
+int error = NO_ERROR;
+
+
+/*
+ * Button Interaction
+ */
 
 int normalizeButtonOrder[] = {15, 11, 7, 3, 14, 10, 6, 2, 13, 9, 5, 1, 12, 8, 4, 0};
 int currentButtonPressed = -1;
@@ -18,7 +65,8 @@ int getButtonPressed(){
 
 
 /*
- * Display Interaction */
+ * Display Interaction
+ */
 
 void displayInitialize(){
 	IBridge_init();
@@ -43,9 +91,13 @@ void displayWriteString(char* text){
 
 
 /*
- * Visuals */
+ * Visuals
+ *
+ * Available characters:
+ * ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ ] _ ^
+ */
 
- void displayWelcomeScreen(){
+void renderWelcomeScreen(){
  	displayWriteString("              ");
  	displayWriteString(" MysteryCrypt ");
  	displayWriteString(" by @cdn      ");
@@ -54,10 +106,21 @@ void displayWriteString(char* text){
  	displayWriteString(" key to start ");
  }
 
-// ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ ] _ ^
-void displayRoomWithContent(char* firstRow, char* secondRow){
+void renderMessage(char* firstRow, char* secondRow){ // Both with exactly 8 characters
+ 	displayClear();
+ 	displayWriteString(" ____________ ");
+ 	displayWriteString(" I          I ");
+ 	displayWriteString(" I ");
+ 		displayWriteString(firstRow);
+ 		displayWriteString(" I ");
+ 	displayWriteString(" I ");
+ 		displayWriteString(secondRow);
+ 		displayWriteString(" I ");
+ 	displayWriteString(" I__________I ");
+ 	displayWriteString("              ");
+ }
+void renderRoomWithContent(char* firstRow, char* secondRow){
 	displayClear();
-	//								 "--------------"
 	displayWriteString(" %          / ");
 	displayWriteString("   +------+   ");
 	displayWriteString("   I");
@@ -67,22 +130,43 @@ void displayRoomWithContent(char* firstRow, char* secondRow){
 	  displayWriteString(secondRow);
 		displayWriteString("I   ");
 	displayWriteString("   +------+   ");
-	displayWriteString(" /          % ");
+
+	char x = currentLocation[0] + '0';
+	char y = currentLocation[1] + '0';
+	displayWriteString(" /   ");
+		displayWriteChar(x);
+		displayWriteString("  ");
+		displayWriteChar(y);
+		displayWriteString("   % ");
 }
 
-void displayWall(){
-	displayRoomWithContent("//////", "//////");
+void renderWall(){
+	renderRoomWithContent("//////", "//////");
 }
-void displayDoor(){
-	displayRoomWithContent("      ", "   @  ");
+void renderEmpty(){
+	renderRoomWithContent("      ", "      ");
+}
+void renderSpecial(){
+	renderRoomWithContent("  ?   ", "   @  ");
+}
+
+void renderError(){
+	displayClear();
+	switch(error){
+		case 0:
+			renderMessage("  Wall  ", " ahead! ");
+			break;
+		default:
+			renderMessage(" ERROR! ", " ------ ");
+	}
+
 }
 
 
 /*
- * Location & orientation */
+ * Orientation
+ */
 
-int previousOrientation = -1;
-int orientation = 0; // 0-3
 void setOrientation(int diff){
 	orientation = orientation + diff;
 	if(orientation > 3){
@@ -91,6 +175,7 @@ void setOrientation(int diff){
 		orientation = 3;
 	}
 }
+
 void setOrientationFromButton(int button){
  	switch(button){
  		case 4:
@@ -101,7 +186,6 @@ void setOrientationFromButton(int button){
  			break;
  	}
  }
-
 boolean detectAndRememberOrientationChange(){
 	if(previousOrientation != orientation){
 		previousOrientation = orientation;
@@ -109,26 +193,135 @@ boolean detectAndRememberOrientationChange(){
 	}
 	return false;
 }
-void displayCurrentLocationWithOrientation(int orientation) {
-	switch(orientation){
-		case 0:
-			displayWall();
-			break;
-		case 1:
-			displayWall();
-			break;
-		case 2:
-			displayDoor();
-			break;
-		case 3:
-			displayWall();
-			break;
+
+
+/*
+ * Location
+ */
+
+int getThingAtLocation(int x, int y){
+	if(x < 0 || y < 0 || x >= WIDTH || y >= LENGTH){
+		return WALL;
+	}else{
+		return _map[x][y];
 	}
+}
+
+int xForOrientation(int orientation){
+	switch(orientation){
+		case NORTH:
+			return currentLocation[0];
+		case WEST:
+			return currentLocation[0] + 1;
+		case SOUTH:
+			return currentLocation[0];
+		case EAST:
+			return currentLocation[0] - 1;
+	}
+}
+int yForOrientation(int orientation){
+	switch(orientation){
+		case NORTH:
+			return currentLocation[1] - 1;
+		case WEST:
+			return currentLocation[1];
+		case SOUTH:
+			return currentLocation[1] + 1;
+		case EAST:
+			return currentLocation[1];
+	}
+}
+
+int whatAmILookingAt(int orientation){
+	int x = xForOrientation(orientation);
+	int y = yForOrientation(orientation);
+	return getThingAtLocation(x, y);
+}
+boolean moveInDirection(int orientation){
+	int x = xForOrientation(orientation);
+	int y = yForOrientation(orientation);
+	if(getThingAtLocation(x, y) == WALL){
+		return false;
+	}else{
+		currentLocation[0] = x;
+		currentLocation[1] = y;
+		return true;
+	}
+}
+
+void render() {
+	int thing = whatAmILookingAt(orientation);
+	switch(thing){
+		case WALL:
+			renderWall();
+			break;
+		case EMPTY:
+			renderEmpty();
+			break;
+		case SPECIAL:
+			renderSpecial();
+			break;
+		// default:
+			// Log error…?
+	}
+}
+
+void setLocationFromButton(int button){
+	switch(button){
+		case 1:
+		case 2:
+			if(!moveInDirection(orientation)){
+				displayError(0);
+			}
+			break;
+		// default:
+			// Error logging
+	}
+}
+boolean detectAndRememberLocationChange(){
+	if(previousLocation[0] == currentLocation[0] &&
+	   previousLocation[1] == currentLocation[1]){
+		return false;
+	}else{
+		previousLocation[0] = currentLocation[0];
+    previousLocation[1] = currentLocation[1];
+		return true;
+	}
+}
+
+boolean handleButton(int button){
+	boolean rerender = false;
+
+	setOrientationFromButton(button);
+	rerender = rerender || detectAndRememberOrientationChange();
+
+	setLocationFromButton(button);
+	rerender = rerender || detectAndRememberLocationChange();
+
+	return rerender;
 }
 
 
 /*
- * Main Logic */
+ * Error Displaying
+ */
+
+void displayError(int errorCode){
+	error = errorCode;
+}
+boolean showAndResetError(){
+	if(error != NO_ERROR){
+		renderError();
+		delay(1000);
+		error = NO_ERROR;
+		return true;
+	}
+	return false;
+}
+
+/*
+ * Main Logic
+ */
 
 void setup(){
   displayInitialize();
@@ -136,12 +329,7 @@ void setup(){
 
 void loop()
 {
-  while(1){
-    int button = getButtonPressed();
-		setOrientationFromButton(button);
-
-		if(detectAndRememberOrientationChange()){
-			displayCurrentLocationWithOrientation(orientation);
-		}
-  }
+	if(handleButton(getButtonPressed()) || showAndResetError()){
+		render();
+	}
 }
